1d9fe65b25ace0978804aaa812f3ee1df2ab2361
sDaman830 <99036076+sDaman830@users.noreply.github.com>
2023-06-05 12:08:40 +0530

    img

---
+++ flappybird.js
@@ -0,0 +1,14 @@
+// board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    boardWidth = boardWidth;
+
+}
+// bridie
+let birdWidth = 34;// ratio of actal img remains sme
+let birdHeight = 24;
+
